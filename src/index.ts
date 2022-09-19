29c84e2d80e9cf2d80e50da3227b839687e38f60
MARTINBROS\bkohlmeier <bkohlmeier@martinbros.com>
2022-09-19 12:03:37 -0400

    Added eslint ignore in webpack config.

--- src/index.ts
+++ src/index.ts
@@ -16,26 +16,3 @@ function startGame() {
 		document.querySelector("#gameAreaContainer")?.classList.remove("hide");
 	}
 }
-
-// class Coffee {
-// 	protected power: number;
-
-// 	constructor(power:number) {
-// 		this.power = power;
-// 	}
-
-// 	getPower() {
-// 		return this.power;
-// 	}
-// }
-
-// class badCoffee extends Coffee {
-// 	_power = 90;
-// }
-
-// const goodCoffee = new Coffee(15);
-// //console.log("good", goodCoffee.getPower());
-// //console.log(goodCoffee.power);
-// const mrCoffee = new badCoffee(85);
-// console.log("bad", mrCoffee.getPower());
-// console.log(mrCoffee._power);
